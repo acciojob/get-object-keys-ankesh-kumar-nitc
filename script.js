bec7a17f5bab669705e5302524c64a57b075da91
ANKESH KUMAR <123188638+ankesh-kumar-nitc@users.noreply.github.com>
2026-07-03 14:22:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// Create the student object
 const student = {
   name: "John"
 };
 
-// Function to get all keys from an object
 function getKeys(obj) {
   return Object.keys(obj);
 }
-
-// Example test
-console.log(getKeys(student)); // ["name"]
