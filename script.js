411bfe651310933ddeff420a075d4e0977390a2a
ANKESH KUMAR <123188638+ankesh-kumar-nitc@users.noreply.github.com>
2026-07-03 14:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,26 +24,3 @@ const student2 = {
   city: "New York",
 };
 console.log(getKeys(student2)); // Output: ["name", "age", "city"]
-
-// student-keys.js
-
-// 1. Create the student object
-const student = {
-  name: "John",
-};
-
-// 2. Create the getKeys function
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
-// 3. Quick manual test
-console.log(getKeys(student)); // ["name"]
-
-// 4. Export for Node/Cypress environments (optional, safe to include)
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = { student, getKeys };
-}
-const { student, getKeys } = require("./student-keys.js");
-
-console.log(getKeys(student)); // ["name"]
